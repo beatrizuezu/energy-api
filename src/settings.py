9a15afd6c9b9e0bcf7d73aa3fe314739e2b21107
Beatriz Uezu <beatriz.uezu@gmail.com>
2024-10-14 21:18:09 +0200

    fix app name

--- src/settings.py
+++ src/settings.py
@@ -16,7 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'src',
+    'src.devices',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +60,12 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
--- src/urls.py
+++ src/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from src.devices.api.views import api
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', api.urls),
 ]
